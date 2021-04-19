3e7edc66340f8fd4a0b9e6547d365d115730185b
jagaamaae <61912510+jagaamaae@users.noreply.github.com>
2021-04-19 11:21:22 -0500

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,131 +1,66 @@
-from model import db, User,  Country, CountryStats, connect_to_db 
-# US,Country
+"""Server for movie ratings app."""
+from flask import (Flask, render_template, request, flash, session,
+                   redirect, jsonify)
+from model import User, Country, CountryStats, connect_to_db
+import crud
 
-"""CRUD operations."""
+from jinja2 import StrictUndefined
 
-"""CRUD operations."""
-
-from model import db, User, CountryStats, Country, connect_to_db 
-
-def create_user(email, password):
-    """Create and return a new user."""
-
-    user = User(email=email, password=password)
-
-    db.session.add(user)
-    db.session.commit()
-
-    return user
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
-def create_user(email, password):
-    """Create and return a new user."""
+@app.route('/')
+def homepage():
+    """View homepage."""
+    return render_template('homepage.html')
 
-    user = User(email=email, password=password)
+@app.route('/countries')
+def show_countries():
+    countries = crud.get_countries()
+    return render_template('all_countries.html', countries=countries)
 
-    db.session.add(user)
-    db.session.commit()
+@app.route('/countries/<country>')
+def show_details(country):
+    population=crud.get_population_by_country(country)
+    cases=crud.get_country_cases(country)
+    return render_template('country_details.html', cases = cases, population=population, country=country,)
 
-    return user
+@app.route('/users/<email>')
+def show_users_details(email):
+    user=crud.get_user_by_email(email)
+    return render_template('user_details.html', user=user)
 
-def create_reference(country,  population):
+@app.route("/users", methods = ["POST"])
+def register_user():
+    email=request.form.get('email')
+    password=request.form.get('password')
+    user = crud.get_user_by_email(email)
+    if user:
+        flash('Cannot create an account with that email. Try again.')
+    else:
+        crud.create_user(email, password)
+        flash('Account created! Please log in.')
 
-    reference= Country(country=country, population = population)
+    return redirect('/')
+    
+@app.route("/login-info", methods = ['POST'])
+def login_info():
+    email=request.form.get('email')
+    password=request.form.get('password')
+    user = crud.get_user_by_email(email)
 
-    db.session.add(reference)
-    db.session.commit()
+    if user and user.password == password:
+        flash('Logged in!') 
+        session['email'] = user.email
+        session['user_id'] = user.user_id
+        return redirect(f'users/{email}')
+    else:
+        flash("Either email or password don't match")
+    return redirect('/')
 
-    return reference
-
-def create_country(date,country,confirmed,recovered, deaths):
-
-    country = CountryStats(date=date,
-                                 country=country,
-                                 confirmed=confirmed, 
-                                 recovered=recovered,
-                                 deaths=deaths)
-
-    db.session.add(country)
-    db.session.commit()
-
-    return country
-
-
-def get_users():
-    """Return all users."""
-
-    return User.query.all()
-
-
-def get_user_by_id(user_id):
-    """Return a user by primary key."""
-    return User.query.get(user_id)
-
-
-def get_user_by_email(email):
-    """Return a user by email."""
-    return User.query.filter(User.email == email).first()
-
-
-def get_countries():
-    """Return all movies."""
-    return Country.query.all()
-
-def get_population_by_country(country):
-    population = Country.query.filter(Country.country == country).first().population
-    return population
-
-def get_country_cases(country):
-    cases = CountryStats.query.filter(CountryStats.country==country).all()
-    return cases
 
 if __name__ == '__main__':
-    from server import app
     connect_to_db(app)
-
-
-
-
-# def create_us(date, country, state, confirmed, deaths ):
-
-#     us = US (date=date,
-#                                  country=country,
-#                                  state=state, 
-#                                  confirmed=confirmed, 
-#                                  deaths=deaths)
-
-#     db.session.add(us)
-#     db.session.commit()
-
-#     return us
-
-# def create_all_country(date, confirmed, recovered, deaths, country, province_state):
-
-#     all_country = Stat(date=date,
-                                 
-#                                  confirmed=confirmed, 
-#                                  recovered=recovered,
-#                                  deaths=deaths,
-#                                  country =country,
-#                                  province_state=province_state
-#                                  )
-
-#     db.session.add(all_country)
-#     db.session.commit()
-
-#     return all_country
-# def create_all_country(date, confirmed, recovered, deaths, country, province_state):
-
-#     all_country = Stat(date=date,
-                                 
-#                                  confirmed=confirmed, 
-#                                  recovered=recovered,
-#                                  deaths=deaths,
-#                                  country =country,
-#                                  province_state=province_state
-#                                  )
-
-#     db.session.add(all_country)
-#     db.session.commit()
-
-#     return all_country
+    app.run(host='0.0.0.0', debug=True)
