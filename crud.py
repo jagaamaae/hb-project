da004c82f524ce71d1afd5cdad6ac6df64e532b8
jagaamaae <61912510+jagaamaae@users.noreply.github.com>
2021-04-19 11:20:07 -0500

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,11 +1,11 @@
-from model import db, User, Stat, Country, connect_to_db 
+from model import db, User,  Country, CountryStats, connect_to_db 
 # US,Country
 
 """CRUD operations."""
 
 """CRUD operations."""
 
-from model import db, User, Stat, Country, connect_to_db 
+from model import db, User, CountryStats, Country, connect_to_db 
 
 def create_user(email, password):
     """Create and return a new user."""
@@ -16,6 +16,39 @@ def create_user(email, password):
     db.session.commit()
 
     return user
+
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+def create_reference(country,  population):
+
+    reference= Country(country=country, population = population)
+
+    db.session.add(reference)
+    db.session.commit()
+
+    return reference
+
+def create_country(date,country,confirmed,recovered, deaths):
+
+    country = CountryStats(date=date,
+                                 country=country,
+                                 confirmed=confirmed, 
+                                 recovered=recovered,
+                                 deaths=deaths)
+
+    db.session.add(country)
+    db.session.commit()
+
+    return country
 
 
 def get_users():
@@ -26,14 +59,13 @@ def get_users():
 
 def get_user_by_id(user_id):
     """Return a user by primary key."""
-
     return User.query.get(user_id)
 
 
 def get_user_by_email(email):
     """Return a user by email."""
-
     return User.query.filter(User.email == email).first()
+
 
 def get_countries():
     """Return all movies."""
@@ -43,59 +75,16 @@ def get_population_by_country(country):
     population = Country.query.filter(Country.country == country).first().population
     return population
 
-def create_user(email, password):
-    """Create and return a new user."""
-
-    user = User(email=email, password=password)
-
-    db.session.add(user)
-    db.session.commit()
-
-    return user
-
-def create_reference(country, lat, long, population):
-
-    reference= Country(country=country, lat=lat, long=long, population = population)
-
-    db.session.add(reference)
-    db.session.commit()
-
-    return reference
-
-def create_all_country(date, confirmed, recovered, deaths, country, province_state):
-
-    all_country = Stat(date=date,
-                                 
-                                 confirmed=confirmed, 
-                                 recovered=recovered,
-                                 deaths=deaths,
-                                 country =country,
-                                 province_state=province_state
-                                 )
-
-    db.session.add(all_country)
-    db.session.commit()
-
-    return all_country
-
+def get_country_cases(country):
+    cases = CountryStats.query.filter(CountryStats.country==country).all()
+    return cases
 
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
 
 
-# def create_country(date,country,confirmed,recovered, deaths):
 
-#     country = Country(date=date,
-#                                  country=country,
-#                                  confirmed=confirmed, 
-#                                  recovered=recovered,
-#                                  deaths=deaths)
-
-#     db.session.add(country)
-#     db.session.commit()
-
-#     return country
 
 # def create_us(date, country, state, confirmed, deaths ):
 
@@ -110,6 +99,21 @@ if __name__ == '__main__':
 
 #     return us
 
+# def create_all_country(date, confirmed, recovered, deaths, country, province_state):
+
+#     all_country = Stat(date=date,
+                                 
+#                                  confirmed=confirmed, 
+#                                  recovered=recovered,
+#                                  deaths=deaths,
+#                                  country =country,
+#                                  province_state=province_state
+#                                  )
+
+#     db.session.add(all_country)
+#     db.session.commit()
+
+#     return all_country
 # def create_all_country(date, confirmed, recovered, deaths, country, province_state):
 
 #     all_country = Stat(date=date,
